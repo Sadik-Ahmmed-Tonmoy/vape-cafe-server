98b5ebdf053fc77b17db070d154abe01595b6d4e
Sadik-Ahmmed-Tonmoy <100148160+Sadik-Ahmmed-Tonmoy@users.noreply.github.com>
2023-08-24 05:03:17 +0600

    cart

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lw1wxb4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,6 +28,7 @@ async function run() {
 
     const userCollection = client.db("vapeCafe").collection("user");
     const productCollection = client.db("vapeCafe").collection("vapeProducts");
+    const cartCollection = client.db("vapeCafe").collection("cart");
 
 
     // users details.
@@ -49,6 +50,48 @@ async function run() {
       const products = await productCollection.find().toArray();
       res.send(products);
     });
+
+// get data of single category
+app.get("/products/:category", async (req, res) => {
+  const category = req.params.category;
+  const query = {category: category}
+  const products = await productCollection.find(query).toArray(); 
+  res.send(products)
+})
+
+
+// get single product all data
+app.get("/productDetails/:id", async (req, res) => {
+const id = req.params.id;
+const query = {_id: new ObjectId(id)}
+result = await productCollection.findOne(query);
+res.send(result)
+})
+
+// cart data
+// post data to cart
+app.post("/addToCart", async(req, res) => {
+  const cart = req.body;
+  const result = await cartCollection.insertOne(cart);
+  res.send(result)
+})
+// get data from cart
+app.get("/cart/data/:email", async(req, res) => {
+  const email = req.params.email;
+  const filter = {email: email}
+  const result = await cartCollection.find(filter).toArray();
+  res.send(result)
+})
+// delete cart
+app.delete("/cart/delete/:id", async(req, res) => {
+  const id = req.params.id
+  const filter = {_id: new ObjectId (id)}
+  const result = await cartCollection.deleteOne(filter)
+  res.send(result)
+})
+
+
+
 
     await client.db("admin").command({ ping: 1 });
     console.log(
